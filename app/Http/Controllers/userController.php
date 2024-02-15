c4655aa88e66df6e1100109807af6097c38628fe
Basharat Ali <basharatalidogar14@gmail.com>
2024-02-15 17:54:48 +0500

    edit readme and add new.md

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -15,4 +15,35 @@ class userController extends Controller
                 return view('welcome',compact('students'));
 
                     }
+                    // Union of two tables...
+    public function uniontables(){
+        $teachers=DB::table('teachers')
+                            ->join('cities','teachers.city','=','cities.id')
+                            ->select('teachers.*','cities.city_name');
+
+        $students=DB::table('student')
+                            ->union($teachers)
+                            ->join('cities','student.city','=','cities.id')
+                            ->select('student.*','cities.city_name')
+                            ->get();
+
+        return view('welcome' ,compact('students'));
+    }
+    public function when(){
+        $students=DB::table('student')
+                         ->when(true, function($query){
+                            $query->where('age','>','20');
+                         })
+                         ->get();
+                         return $students;
+    }
+    public function chunk(){
+        $students=DB::table('student')
+                           ->orderBy('id')
+                           ->chunk(3, function($students){
+                            foreach($students as $student){
+                                echo $student->name . "<br>";
+                            }
+                        });
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 
 Route::get('/',[userController::class,'studentData'])->name('home');
-
+Route::get('/union',[userController::class,'uniontables'])->name('union');
+Route::get('/when',[userController::class,'when'])->name('when');
+Route::get('/chunk',[userController::class,'chunk'])->name('chunk');
